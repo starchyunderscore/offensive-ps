c78bd67c871ec7437c61e69c13b7e83024bd10dc
starchyunderscore <spenceramay22@gmail.com>
2023-10-24 09:53:25 -0400

    Update payload.ps1

--- method-1/payload.ps1
+++ method-1/payload.ps1
@@ -1,10 +1,2 @@
 Start-Sleep -Seconds 20
 calc
-<#
-# What this could do instead of just opening the calculator:
-# Download an exe and run it
-# Download an exe and replace another exe with it, so that the next time that program is run it runs the payload
-# Copy all of a user's sensitive data to a folder, zip it, and send that zip to a server
-# Edit the startup of powershell to download and run a script, so that the attacker can run an updated info-stealer every time powershell is launched
-# Almost anything else you can think of
-#>
